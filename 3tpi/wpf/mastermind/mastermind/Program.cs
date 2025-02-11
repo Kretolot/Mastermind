fd81fbec4dee761a4ca0ed49e0030e5a18ea5bf0
Tomasz Gradowski <gomekrytel@gmail.com>
2025-02-11 14:50:05 +0100

    zadanie leszek2

---
+++ 3tpi/wpf/mastermind/mastermind/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] kolory = { "czerwony", "niebieski", "zielony", "żółty", "pomarańczowy", "fioletowy", "szary", "czarny" };
+
+        Random random = new Random();
+
+        for (int i = 0; i < 20; i++)
+        {
+            string[] wylosowaneBez = kolory.OrderBy(_ => random.Next()).Take(4).ToArray();
+            Console.WriteLine($" Losowanie {i}: " + string.Join(", ", wylosowaneBez));
+        }
+
+        for (int i = 0; i < 20; i++)
+        {
+            string[] wylosowaneZ = Enumerable.Range(0, 4).Select(_ => kolory[random.Next(kolory.Length))] ;
+        }
+    }
+}
